68ecb2b978a246ccdb7d6e8ab02cd184ae98a8a3
dianamin <97.diana@gmail.com>
2014-03-26 18:57:08 +0200

    Cub 3D

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer(); 
 renderer.setSize( window.innerWidth, window.innerHeight ); 
+renderer.shadowMapSoft = true;
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.CubeGeometry(1,1,1); 
@@ -10,10 +11,15 @@ var cube = new THREE.Mesh( geometry, material );
 scene.add( cube ); 
 camera.position.z = 5;
 
+cube.rotation.y -= 10;
+cube.castshadow = true;
+cube.recieveshadow = true;
+
+
 
 function render() { 
 	requestAnimationFrame(render); 
-	renderer.render(scene, camera); 
+	renderer.render(scene, camera);
 } 
 
 render();
